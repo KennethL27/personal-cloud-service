e23a0ffa1eda1cf8bb11f5d9879a20b2aec664b7
KennethL27 <kenneth.lara01@gmail.com>
2025-03-23 16:16:52 -0700

    Add negative test for health endpoint

--- tests/api/test_health_check.py
+++ tests/api/test_health_check.py
@@ -1,5 +1,24 @@
+from unittest.mock import patch, MagicMock
+
 class TestHealthCheck:
     def test_health_check(self, test_client):
         response = test_client.get("/health/")
         assert response.status_code == 200
         assert response.json() == {"status": "ok"}
+
+    def test_health_check__fail(self, test_client):
+        response = test_client.get("/health/bad")
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Not Found"}
+
+    @patch("src.api.health_check.health_check")
+    def test_health_check_unexpected_error(self, test_client):
+        response_mock = MagicMock()
+        response_mock.status_code = 500
+        response_mock.json.return_value = {"error": "Unexpected error"}
+
+        test_client.get = MagicMock(return_value=response_mock)
+        response = test_client.get("/health/")
+        
+        assert response.status_code == 500
+        assert "error" in response.json()
