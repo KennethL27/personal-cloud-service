d036c7e0bb882b6ed10762142d85852197c78371
KennethL27 <kenneth.lara01@gmail.com>
2025-04-03 11:50:00 -0500

    Adjust main.py
    In this commit, we are updating how
    main.py adds endpoints to the router.
    This method now adds endpoints
    dynamically. Therefore, when new
    endpoints are created, main.py doesn't need to be touched

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
-from .api import health_check, file_upload
+from pathlib import Path
+import pkgutil
+import importlib
 
 app = FastAPI(title = "Personal Cloud Service")
-app.include_router(health_check.router)
-app.include_router(file_upload.router)
+
+def include_routers():
+    package = "src.api"
+    api_path = Path(__file__).parent / "api"
+
+    for module_info in pkgutil.walk_packages([str(api_path)], prefix=f"{package}."):
+        module = importlib.import_module(module_info.name)
+        
+        if hasattr(module, "router"):
+            # Convert module path to URL prefix (e.g., "app.api.file.upload" -> "/file/upload")
+            relative_path = module_info.name.replace(package + ".", "").replace(".", "/")
+            prefix = f"/{relative_path}"
+
+            app.include_router(module.router, prefix=prefix)
+            
+include_routers()
 
 @app.get('/')
 async def root():
